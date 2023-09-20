82c3183612e4b208794808e7306e1bd3d1bbcdb2
juanbike <juancarlos.arteaga1@gmail.com>
2023-09-20 08:28:20 -0400

    Crud de juntas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import {  map } from 'rxjs';
 import { Junta } from './model/products';
 import { v4 as uuidv4 } from 'uuid';
-import { JuntasService } from './juntas.service';
+//import { JuntasService } from './juntas.service';
 
 
 @Component({
@@ -12,14 +12,25 @@ import { JuntasService } from './juntas.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+
+
+
   title = 'AngularHttpRequest';
   private apiurl="http://localhost:3500/api/juntas/";
-  allJuntas: Junta[];
-  datosTabla: Junta[] ;
-  httpClient: HttpClient;
+  allJuntas: Junta[] = [];
+  //datosTabla: Junta[] ;
 
-  constructor(private juntasService: JuntasService, httpClient: HttpClient) {}
+  showMessage: boolean = false;
+  messageText: string = '';
 
+  constructor(private http: HttpClient){}
+
+
+  //httpClient: HttpClient;
+
+  //constructor(private juntasService: JuntasService, httpClient: HttpClient) {}
+
+  /*
   private fethcjuntas(){
     this.juntasService.obtenerDatosTabla().subscribe((datos) => {
       this.datosTabla = datos;
@@ -27,6 +38,7 @@ export class AppComponent implements OnInit{
       this.allJuntas = datos
     });
   }
+*/
 
 
 
@@ -34,25 +46,48 @@ export class AppComponent implements OnInit{
 
 
   ngOnInit(): void {
-      this.fethcjuntas();
+      this.fetchJuntas();
   }
 
+
   onJuntas(){
-    this.fethcjuntas();
+    this.fetchJuntas();
   }
+
 
 
 
   onJuntaCreate(juntas:{ tipo_extremos: string, tipo_material: string, material: string}){
     console.log(juntas);
+    this.http.post<Junta>(this.apiurl, juntas).subscribe( (response)  =>{
+      console.log(response);
+    }
+      )
+    /*
     const headers = new HttpHeaders({ 'myHeaders': "juanbike"})
     this.httpClient.post(this.apiurl, juntas).subscribe(
       (response) =>{
         console.log(response)
       }
     );
+    */
   }
 
+
+  private fetchJuntas(){
+    this.http.get(this.apiurl).pipe(
+      map((response) => {
+        return response as Junta[];
+      })
+
+    )
+    .subscribe((response) => {
+      console.log(response);
+      this.allJuntas = response;
+      return response;
+    })
+
+  }
 
   /*
   private fethcjuntas(){
@@ -86,12 +121,60 @@ export class AppComponent implements OnInit{
 
   */
 
-  onDeleteJunta(id: string){
-    console.log(id.toString());
-    //const idt = +id + 1
-    //console.log('idt:'+idt);
-    this.httpClient.delete(`${this.apiurl}/${id.toString()}`).subscribe()
+
+  onDeleteJunta(id: string):void{
+
+    this.http.delete<void>(`${this.apiurl}${id}`).subscribe(
+      () => {
+        // Operación exitosa
+        this.showMessageWithTimeout('Junta eliminada con éxito', 3000);
+
+      },
+      (error: HttpErrorResponse) =>{
+        console.log('Error al eliminar la junta', error);
+        // Muestro mensaje de error
+        this.showMessageWithTimeout('Error al eliminar la junta', 3000);
+
+      }
+    );
   }
+
+  showMessageWithTimeout(message: string, timeout: number): void {
+    this.showMessage = true;
+    this.messageText = message;
+
+    setTimeout(() => {
+      this.showMessage = false;
+      this.messageText = '';
+    }, timeout);
+  }
+
+
+
+  
+
+  /*
+
+  onDeleteJunta(id: string): void {
+    const url = `${this.apiurl}/${id}`;
+
+    this.httpClient.delete(url).subscribe(
+      () => {
+        console.log(`Junta con ID ${id} eliminada con éxito.`);
+        // Realizar cualquier acción adicional después de eliminar, como actualizar la lista de juntas.
+      },
+      (error) => {
+        console.error(`Error al eliminar la junta con ID ${id}: ${error}`);
+        // Manejar errores aquí, por ejemplo, mostrar un mensaje de error al usuario.
+      }
+    );
+  }
+*/
+
+
+
+
+
 
 
 
