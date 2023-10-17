0442f63da8122c13a1f6a2a209f70f6145996b96
juanbike <juancarlos.arteaga1@gmail.com>
2023-10-17 17:08:29 -0400

    creamos un servicio para los componentes agregar, editar y eliminar

--- src/app/model/products.ts
+++ src/app/model/products.ts
@@ -5,23 +5,11 @@ export class Junta{
     price: string
     id?: string;
   */
-    nominal: string;
-    nominal1: string;
-    lineaOSistema: string;
-
-    especificacion: string;
-    schedule: string;
     tipo_extremos: string;
-
     tipo_material: string;
     material: string;
-    diam_inch_contabilizadas: string;
-
-    factor_pulgadas_diametrales: string;
-    pulgadas_diametrales: string;
-
-    proyectID: string;
-    usuarioID: string;
-
     id?: string;
+	
 }
+
+
